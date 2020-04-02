4f95378f11a3ee34076a9340f20cc6c8facddab6
dario <dario.diaz@alumno.udg.mx>
2020-04-01 23:20:07 -0600

    Código pre-final

--- ReproductorDeAudio/Applications/app_Debounce.c
+++ ReproductorDeAudio/Applications/app_Debounce.c
@@ -20,15 +20,12 @@ T_UBYTE rub_ShortPress = FALSE;
 T_UBYTE rub_LongPress = FALSE;
 T_UBYTE rub_Button[NUMBERS_BUTTON] = {1,1,1};
 T_UBYTE rub_ButtonsStates[NUMBERS_BUTTON];
-T_UBYTE rub_States[4];
+T_UBYTE rub_States[3];
 T_UBYTE rub_lub_o = 0u;
 T_UBYTE lub_i = 0u;
 T_UBYTE lub_o[NUMBERS_BUTTON] = {0,0,0};
-T_UBYTE rub_StatesFalse[4];
 T_UBYTE rub_PausePlay;
 T_UBYTE rub_StopRotabit = FALSE;
-T_UBYTE rub_LongPressFlag = FALSE;
-T_UBYTE rub_NormalPress = FALSE;
 
 void app_DebounceTask(void)
 {
@@ -40,52 +37,39 @@ void app_DebounceTask(void)
 void app_DebounceValues(void)
 {
 	lub_i = 0u;
-	T_UBYTE lub_UpDown;
-	T_UBYTE lub_Entry = 0u;
-//	T_UBYTE lub_UpDown = FALSE;
+	T_UBYTE lub_UpDown = FALSE;
 	app_ReadInputValue();
 	while(lub_i < NUMBERS_BUTTON)
 	{
 
-		if((lub_o[lub_i] > 50) && (lub_o[lub_i] < 200) && ((lub_ButtonState[lub_i] == 1)))
+		if((lub_o[lub_i] > 200) && (lub_ButtonState[lub_i] == 1))
 		{
 			rub_Button[lub_i] = 0u;
 			lub_i = lub_i + 1;
 			lub_UpDown = TRUE;
 		}
-		if((lub_o[lub_i] > 200) && (lub_ButtonState[lub_i] == 1))
-		{
-			if(lub_ButtonState[2] == 1)
-			{
-				rub_States[lub_i] = LONGPRESS;
-				lub_i = lub_i + 1;
-			}
-			else
-			{
-				rub_Button[lub_i] = 1u;
-				lub_i = lub_i + 1;
-			}
-		}
+		if((lub_o[lub_i] > 50) && (lub_o[lub_i] < 200) && (lub_ButtonState[lub_i] == 1))
+				{
+					rub_Button[lub_i] = 0u;
+					lub_i = lub_i + 1;
+					lub_UpDown = TRUE;
+				}
 		if(lub_ButtonState[lub_i] == 1)
 				{
 					rub_Button[lub_i] = lub_ButtonState[lub_i];
 					lub_i = lub_i + 1;
 				}
-
 		else
 		{
-			lub_Entry = lub_Entry + 1;
 			for(lub_o[lub_i]=0u;(lub_ButtonState[lub_i] == 0) && (lub_UpDown == FALSE); lub_o[lub_i]++)
 			{
 
-			if((rub_flagPIT1 == TRUE) && (lub_i == 0) && (lub_o[1] > 200))
+			if((rub_flagPIT1 == TRUE) && (lub_i == 1) && (lub_o[1] > 200))
 			{
 
 				app_FOWARD();
 				lub_o[lub_i] = lub_o[lub_i] + 1;
 				rub_flagPIT1 = FALSE;
-
-
 			}
 			if((rub_flagPIT1 == TRUE) && (lub_i == 0) && (lub_o[0] > 200))
 			{
@@ -113,15 +97,16 @@ void app_DebounceSelecction(void)
 	lub_i = 0;
 		while(lub_i < NUMBERS_BUTTON)
 		{
-
+				if((rub_Button[lub_i] == 0) && (lub_o[lub_i] > 200))
+				{
+					rub_States[lub_i] = LONGPRESS;
+				}
 				if((rub_Button[lub_i] == 0) && (lub_o[lub_i] > 50) && (lub_o[lub_i] < 199))
 				{
-//					lub_o[lub_i] = 0u;
 					rub_States[lub_i] = PRESS;
 				}
 				else if(rub_Button[lub_i] == 1)
 				{
-//					lub_o[lub_i] = 0u;
 					rub_States[lub_i] = NOTPRESS;
 				}
 				lub_i = lub_i + 1;
@@ -140,7 +125,6 @@ void app_DebounceStages(void)
 			}
 			if(rub_States[lub_i] == PRESS)
 			{
-				rub_LongPressFlag = FALSE;
 				switch (rub_ButtonsStates[lub_i]) {
 					case 0: {
 						if(rub_PausePlay == TRUE)
